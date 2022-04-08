d7e8ebe9f9a4bf6141253c8615caa5d344431911
Akhil24-abd <akghnekar2020@gmail.com>
2022-04-08 12:42:14 +0530

    commit6

--- sw.js
+++ sw.js
@@ -23,7 +23,8 @@ self.addEventListener('fetch', e => {
   );
   });*/
 
-var element = document.querySelector("btn").addEventListener("click", async () => {
+    var element = document.querySelector("btn");
+    element.addEventListener("click", async () => {
      var swRegistration = await navigator.serviceWorker.register("sw.js");
      swRegistration.sync.register("helloSync").then(function () {
      console.log("Sync success");
