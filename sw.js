0a58dba60eb7360c56332b00d5e9afc0bfdb4ec3
Akhil24-abd <akghnekar2020@gmail.com>
2022-04-08 16:05:59 +0530

    commit11

--- sw.js
+++ sw.js
@@ -44,3 +44,19 @@ self.addEventListener('activate' , ()=>{
     console.log("sw is Activated")
 })
 
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          // The responce is in the cache
+          if (response) {
+            return response;
+          }
+  
+          // No cache match, we attempt to fetch it from the network
+          return fetch(event.request);
+        }
+      )
+    );
+  })
+  
