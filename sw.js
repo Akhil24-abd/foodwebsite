93afea34e3a903cf544455579fddbc134733e128
Akhil24-abd <akghnekar2020@gmail.com>
2022-04-29 16:03:06 +0530

    commmaster

--- sw.js
+++ sw.js
@@ -59,4 +59,30 @@ self.addEventListener('fetch', (event) => {
       )
     );
   })
+
+  var cacheName = 'blog-img-1.jpg';
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+  e.respondWith(
+  fetch(e.request)
+  .then(res => {
+  // The response is a stream and in order the browser
+  // to consume the response and in the same time the
+  // cache consuming the response it needs to be
+  // cloned in order to have two streams.
+  const resClone = res.clone();
+  // Open cache
+  caches.open(cacheName)
+  .then(cache => {
+  // Add response to cache
+  cache.put(e.request, resClone);
+  });
+  return res;
+  }).catch(
+  err => caches.match(e.request)
+  .then(res => res)
+  )
+  );
+  });
+
   
